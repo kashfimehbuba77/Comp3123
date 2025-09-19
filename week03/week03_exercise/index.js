2cd4de8423ce7616e113ac370954bd8afbda579b
Kashfi Mehbuba <kashfimehbuba71@gmail.com>
2025-09-19 03:08:13 -0400

    Updating the code based on the exercise TODOs

--- week03/week03_exercise/index.js
+++ week03/week03_exercise/index.js
@@ -1,5 +1,6 @@
 var http = require("http");
 //TODO - Use Employee Module here
+const employees = require("./Employee");
 console.log("Lab 03 -  NodeJs");
 
 //TODO - Fix any errors you found working with lab exercise
@@ -14,20 +15,35 @@ const server = http.createServer((req, res) => {
     } else {
         if (req.url === '/') {
             //TODO - Display message "<h1>Welcome to Lab Exercise 03</h1>"
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end("<h1>Welcome to Lab Exercise 03</h1>");
+            return;
         }
 
         if (req.url === '/employee') {
             //TODO - Display all details for employees in JSON format
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(employees));
+            return;
         }
 
         if (req.url === '/employee/names') {
             //TODO - Display only all employees {first name + lastname} in Ascending order in JSON Array
             //e.g. [ "Ash Lee", "Mac Mohan", "Pritesh Patel"]
+            let names = employees.map(emp => `${emp.firstName} ${emp.lastName}`);
+            names.sort();
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(names));
+            return;
         }
 
         if (req.url === '/employee/totalsalary') {
             //TODO - Display Sum of all employees salary in given JSON format 
             //e.g. { "total_salary" : 100 }  
+            let totalSalary = employees.reduce((sum, emp) => sum + emp.Salary, 0);
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ total_salary: totalSalary }));
+            return;
     }
     res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
     }
