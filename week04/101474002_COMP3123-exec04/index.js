ad7df34f09ee86961b6ece15ef72279a0363c65e
Kashfi Mehbuba <kashfimehbuba71@gmail.com>
2025-09-25 16:26:11 -0400

    week04 exercise

---
+++ week04/101474002_COMP3123-exec04/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Serve static files in /public
+app.use(express.static("public"));
+
+// GET /hello
+app.get("/hello", (req, res) => {
+  res.type("text/plain").send("Hello Express JS");
+});
+
+// GET /user?firstname=&lastname=
+app.get("/user", (req, res) => {
+  const firstname = req.query.firstname || "Pritesh";
+  const lastname = req.query.lastname || "Patel";
+  res.json({ firstname, lastname });
+});
+
+// POST /user/:firstname/:lastname
+app.post("/user/:firstname/:lastname", (req, res) => {
+  const { firstname, lastname } = req.params;
+  res.json({ firstname, lastname });
+});
+
+// POST /users (expects array of { firstname, lastname })
+app.post("/users", (req, res) => {
+  const users = Array.isArray(req.body) ? req.body : [];
+  res.json(users);
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
